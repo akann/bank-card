867aff3ce2ef0536f540213a7a0edc88f29e14ea
Akan Nkweini <akan2000@gmail.com>
2015-10-13 21:41:27 +0100

    initial add

---
+++ src/test/java/org/nkweini/akan/CardTest.java
@@ -0,0 +1,16 @@
+package org.nkweini.akan;
+
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by akan on 13/10/2015.
+ */
+public class CardTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+}
